e1e0bc42f19e6b8e8b07d3055b5cc0a46ea4057e
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-03-16 22:18:01 +0545

    Learned to use useAnimationControls hook to perform animation on a div when a button is clicked.

--- next-framer/src/app/framer-playground/page.jsx
+++ next-framer/src/app/framer-playground/page.jsx
@@ -1,9 +1,11 @@
 "use client";
 
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useAnimationControls } from "framer-motion";
 
 const FramerPlayGround = () => {
+  const rotateControls = useAnimationControls();
+
   const [isVisible, setIsVisible] = useState(true);
 
   // Transform values are applied in a specific order: translate, scale, rotate, skew. However, you can customize this default order using the transormTemplate prop
@@ -11,9 +13,15 @@ const FramerPlayGround = () => {
     return `rotate(${rotate}) translateX(${x})`;
   }
 
+  const handleClickRotate = () => {
+    console.log(rotateControls);
+    rotateControls.start("rotateDiv");
+  };
+
   return (
     <div className="min-h-screen bg-red-200 flex justify-center items-center flex-col gap-8 py-8">
-      <motion.button
+      {/* Basic of motion and AnimatePresence */}
+      {/* <motion.button
         className="bg-black text-white px-4 py-2 rounded-lg"
         onClick={() => setIsVisible(!isVisible)}
         layout
@@ -34,7 +42,33 @@ const FramerPlayGround = () => {
             }}
           />
         )}
-      </AnimatePresence>
+      </AnimatePresence> */}
+
+      {/* useAnimationControls */}
+      <motion.button
+        className="bg-black text-white px-4 py-2 rounded-lg"
+        onClick={handleClickRotate}
+      >
+        Rotate
+      </motion.button>
+
+      <motion.div
+        className="bg-blue-400 mx-auto w-80 aspect-square rounded-lg"
+        variants={{
+          initial: {
+            rotate: 0,
+          },
+          rotateDiv: {
+            rotate: "360deg",
+          },
+        }}
+        initial="initial"
+        animate={rotateControls}
+        transition={{
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+      />
     </div>
   );
 };
