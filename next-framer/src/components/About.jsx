4f3abf5eabd087326f7f12db74b4ab9f2d0d4c4d
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-03-23 18:12:38 +0545

    Fixed about's parallax

--- next-framer/src/components/About.jsx
+++ next-framer/src/components/About.jsx
@@ -12,9 +12,9 @@ const About = () => {
           backgroundAttachment: "fixed",
           backgroundImage: `url(/assets/boy-photographer.jpg)`,
           backgroundSize: "cover",
-          backgroundPosition: "right",
+          backgroundPosition: "center",
         }}
-        className="relative min-h-[50dvh] -mt-[80%] md:-mt-[20%]"
+        className="relative min-h-[70dvh] -mt-[80%] md:-mt-[20%]"
       ></div>
       <div className="bg-black py-20">
         <Container>
