11a3b618aa9646839db2fec3404e711d1312b4ec
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-03-23 12:49:22 +0545

    Added a lat lng info box below the hero section

--- next-framer/src/components/Hero.jsx
+++ next-framer/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useRef } from "react";
-import SectionContainer from "./containers/SectionContainer";
+import { FaRegCompass } from "react-icons/fa";
 import Image from "next/image";
 import { useScroll, motion, useTransform } from "framer-motion";
 
@@ -21,12 +21,12 @@ const Hero = () => {
     <>
       <div
         id="hero"
-        className="min-h-screen flex justify-center items-start relative"
+        className="min-h-screen flex justify-center items-center relative"
         ref={ref}
       >
         <motion.h1
           whileInView={{
-            opacity: [0, 1],
+            opacity: [0, 0.8],
             y: [100, 0],
             transition: {
               duration: 0.4,
@@ -35,7 +35,7 @@ const Hero = () => {
           }}
           initial={{ opacity: 0 }}
           style={{ y: textY }}
-          className="font-extrabold text-white uppercase text-7xl md:text-8xl mt-44 relative z-10"
+          className="font-extrabold text-white uppercase text-7xl md:text-9xl relative z-10 -top-32"
         >
           Nishan
         </motion.h1>
@@ -75,8 +75,25 @@ const Hero = () => {
       </div>
       <motion.div
         style={{ y: shutterY }}
-        className="min-h-[200dvh] w-full bg-black relative z-30 blur-[2px]"
-      ></motion.div>
+        className="min-h-[200dvh] px-8 w-full bg-black relative z-30"
+      >
+        <p className="absolute -top-8 text-white flex justify-center items-center gap-2 before:bg-black/70 before:px-4 before:py-4 before:rounded-lg before:w-full before:absolute before:h-full before:-z-20">
+          <motion.span
+            animate={{
+              rotate: "360deg",
+              transition: {
+                duration: 1.8,
+                repeatDelay: 0.5,
+                ease: "easeInOut",
+                repeat: Infinity,
+              },
+            }}
+          >
+            <FaRegCompass size={25} />
+          </motion.span>
+          <span className="text-sm">26°41'58.3"N, 87°17'23.9"E</span>
+        </p>
+      </motion.div>
     </>
   );
 };
