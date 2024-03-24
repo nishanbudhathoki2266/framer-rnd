5e960ae18001e6b2260ac6737801a87e566b1486
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-03-24 11:26:01 +0545

    Changed the dimensions of portfolio card

--- next-framer/src/components/Portfolio.jsx
+++ next-framer/src/components/Portfolio.jsx
@@ -11,14 +11,13 @@ const Portfolio = () => {
 
   const { scrollYProgress } = useScroll({ target: ref });
 
-  const translateX = useTransform(scrollYProgress, [0, 1], ["0%", "-90%"]);
+  const translateX = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
 
   return (
     <div className="container mx-auto min-h-[600dvh] relative" ref={ref}>
       <div className="flex items-center justify-center text-8xl text-center pt-8 text-white/80">
         My Works
       </div>
-
       <motion.div
         animate={{
           top: [0, 25],
@@ -33,7 +32,6 @@ const Portfolio = () => {
       >
         <BsMouse size={50} className="text-white/80" />
       </motion.div>
-
       <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
         <motion.div
           style={{ x: translateX }}
@@ -45,7 +43,7 @@ const Portfolio = () => {
               key={item.id}
             >
               <div className="flex bg-white flex-col px-3 py-4 rounded-md gap-2 justify-center items-center">
-                <div className="h-[60dvh] rounded-md overflow-hidden aspect-square relative">
+                <div className="h-[50vh] aspect-square relative">
                   <Image src={item.src} alt={item.alt} fill objectFit="cover" />
                 </div>
                 <h2 className="text-3xl font-semibold italic">{item.title}</h2>
